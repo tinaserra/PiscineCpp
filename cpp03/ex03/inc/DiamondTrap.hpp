6824f3ee8b79059cae0b28730069e3b6e0ce6d5a
tinaserra <vserra@student.42.fr>
2022-02-09 23:21:16 +0100

    [c'est pas un sprint c'est un marathon

--- cpp03/ex03/inc/DiamondTrap.hpp
+++ cpp03/ex03/inc/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 18:37:30 by vserra            #+#    #+#             */
-/*   Updated: 2022/02/08 19:56:17 by vserra           ###   ########.fr       */
+/*   Updated: 2022/02/09 23:18:56 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,6 @@
 #include <string>
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
-
-// class Guerrier: public PersoFort, public PersoCourageux{
-// public:
-// 	Guerrier(const string& nom, int ptsDeCourage, int ptsDeForce)
-// 		: Personnage(nom), // résout l'ambiguité sur l'initialisation de Personnage
-// 		PersoCourageux(nom, ptsDeCourage),
-// 		PersoFort (nom, ptsDeForce) {}
- 
-// };
- 
 
 class DiamondTrap : public ScavTrap, public FragTrap
 {
