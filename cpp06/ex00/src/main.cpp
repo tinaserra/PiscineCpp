af0e05b83a7cd27e764ed93f6d5f8cb3758c8cff
tinaserra <vserra@student.42.fr>
2022-02-21 12:10:24 +0100

    [update] ex00

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -3,37 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tinaserra <tinaserra@student.42.fr>        +#+  +:+       +#+        */
+/*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 20:54:27 by vserra            #+#    #+#             */
-/*   Updated: 2022/02/20 19:57:54 by tinaserra        ###   ########.fr       */
+/*   Updated: 2022/02/21 12:09:48 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "castType.hpp"
 
-bool	isOutOfRangeDouble(std::string const &str)
-{
-	long double	l = std::strtod(str.c_str(), NULL);
-	if (l < LLONG_MIN || l > LLONG_MAX)
-	{
-		std::cout << "double Out of range !" << std::endl;
-		return false;
-	}
-	return true;
-}
+/*
+ * ./StaticCast 41555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+*/
 
 int		main(int ac, char *av[])
 {
+	if (ac == 1)
+		std::cout << "\033[1;33mMets des arguments ta race!\033[0m" << std::endl;
 	if (ac != 2)
 	{
-		std::cout << "Met des arguments ta race" << std::endl;
+		std::cout << std::endl << "Format: " << std::endl;
+		std::cout << " ----------------- " << std::endl;
+		std::cout << "| Char    | \"'a'\" |" << std::endl;
+		std::cout << "| Int     | 42    |" << std::endl;
+		std::cout << "| Float   | 42.0f |" << std::endl;
+		std::cout << "| Double  | 42.0  |" << std::endl;
+		std::cout << " ----------------- " << std::endl;
 		return (0);
 	}
 	std::string arg(av[1]);
 	if (arg.length() == 0)
 	{
-		std::cout << "Erreur" << std::endl;
+		std::cout << "\033[1;33mArguments vide!\033[0m" << std::endl;
 		return (0);
 	}
 
@@ -43,6 +44,5 @@ int		main(int ac, char *av[])
 		std::cout << "Unknown type" << std::endl;
 	else
 		converters[t](arg);
-	// convert(arg);
 	return (0);
 }
