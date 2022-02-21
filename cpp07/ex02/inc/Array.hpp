d5727b1ba12bf581a7de5bebfcfa684acba9e499
tinaserra <vserra@student.42.fr>
2022-02-21 22:03:18 +0100

    [add] ex02

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Aray.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:58:29 by vserra            #+#    #+#             */
-/*   Updated: 2022/02/21 18:14:22 by vserra           ###   ########.fr       */
+/*   Updated: 2022/02/21 20:33:41 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,29 @@
 
 # include <iostream>
 
+/*
+ * Construction sans paramètres : crée un array vide.
+ * Construction avec un paramètre de type unsigned int n :
+ * crée un array de n éléments initialisés par défaut.
+ * 
+ * Conseil : Essayez de compiler int * a = new int(); puis affichez *a.
+ * 
+ * Construction par recopie et surcharge de l’opérateur d’affectation. Dans les deux
+ * cas, après une copie, toute modification de l’array original ou de sa copie ne doit
+ * pas impacter l’autre array.
+ * 
+ * Vous DEVEZ utiliser l’opérateur new[] pour allouer de la mémoire.
+ * Toute allocation préventive (c’est-à-dire allouer de la mémoire en avance) est interdite.
+ * Votre programme ne doit pas pouvoir accéder à une zone non allouée.
+ * 
+ * Les éléments doivent être accessibles grâce à l’opérateur d’indice : [ ].
+ * 
+ * En cas d’index invalide lors d’une tentative d’accès d’un élément en utilisant l’opérateur [ ], une std::exception est jetée.
+ * 
+ * Une fonction membre size() qui retourne le nombre d’éléments dans l’array.
+ * Cette fonction membre ne prend aucun paramètre et ne doit pas modifier l’instance courante
+*/
+
 template<typename T>
 class	Array
 {
@@ -22,7 +45,7 @@ class	Array
 		/* Constructors & Destructors */
 		Array(void) : _array(NULL), _size(0) {}
 		Array(unsigned int n) : _array(new T[n]), _size(n) {}
-		Array(Array const & src) : _array(NULL), _size(0) { *this = src; }
+		Array(Array const & save) : _array(NULL), _size(0) { *this = save; }
 		~Array(void)
 		{
 			if (_array != NULL)
@@ -56,7 +79,8 @@ class	Array
 		class	OutOfRange : public std::exception
 		{
 			public:
-				virtual const char *what() const throw() {
+				virtual const char *what() const throw()
+				{
 					return ("Out of range !");
 				};
 		};
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -6,61 +6,111 @@
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:56:34 by vserra            #+#    #+#             */
-/*   Updated: 2022/02/21 18:16:04 by vserra           ###   ########.fr       */
+/*   Updated: 2022/02/21 22:02:07 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <Array.hpp>
 #include <stdlib.h>
+#include "Array.hpp"
+#include <iostream>
 
-#define MAX_VAL 750
-int main(int, char**)
+int	main()
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	std::cout << "* \033[1;37m[INT]\033[0m -------------------- *" << std::endl;
+	{
+		Array<int>array(5);
+		Array<int>copy(array);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+		try {
+			for (unsigned int i = 0; i < array.size(); i++)
+				array[i] = i;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;
-    return 0;
+			for (unsigned int i = 0; i < copy.size(); i++)
+				copy[i] = i + 10;
+
+			for (unsigned int i = 0; i < array.size(); i++)
+				std::cout << array[i] << " ";
+
+			std::cout << std::endl;
+
+			for (unsigned int i = 0; i < copy.size(); i++)
+				std::cout << copy[i] << " ";
+
+			std::cout << std::endl;
+
+			std::cout << array[6] << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "* \033[1;37m[FLOAT]\033[0m ------------------ *" << std::endl;
+	{
+		Array<float>array(3);
+		Array<float>copy;
+		
+		float	floats[3] = {9.999, 99.9, 999.99};
+		unsigned int sz = array.size();
+		try {
+			for (unsigned int i = 0; i < sz; i++)
+				array[i] = floats[i];
+
+			copy = array;
+
+			for (unsigned int i = 0; i < sz; i++)
+				std::cout << array[i] << " ";
+			std::cout << std::endl;
+			for (unsigned int i = 0; i < copy.size(); i++)
+				std::cout << copy[i] << " ";
+			std::cout << std::endl;
+
+			std::cout << std::endl;
+
+			for (unsigned int i = 0; i < sz; i++)
+				array[i] = array[i] + 1;
+
+			for (unsigned int i = 0; i < sz; i++)
+				std::cout << array[i] << " ";
+			std::cout << std::endl;
+
+			for (unsigned int i = 0; i < copy.size(); i++)
+				std::cout << copy[i] << " ";
+			std::cout << std::endl;
+
+			std::cout << array[10] << std::endl;
+		}
+		catch (std::exception & e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	
+	std::cout << "* \033[1;37m[STRINGS]\033[0m ---------------- *" << std::endl;
+	{
+		Array<std::string>array(4);
+
+		try {
+			array[0] = "Ce n'est pas";
+			array[1] = "un sprint";
+			array[2] = "mais";
+			array[3] = "un marathon";
+
+			for (unsigned int i = 0; i < array.size(); i++) {
+				std::cout << array[i] << " ";
+			}
+			std::cout << std::endl;
+
+			std::cout << array[12] << std::endl;
+		}
+		catch (std::exception & e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	
+	return (0);
 }
