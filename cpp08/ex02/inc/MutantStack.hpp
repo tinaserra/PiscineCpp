54adf78e297f131f6924a799fe379bc8d666fd6e
tinaserra <vserra@student.42.fr>
2022-02-24 17:07:13 +0100

    [add] ex02

---
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 16:26:45 by vserra            #+#    #+#             */
+/*   Updated: 2022/02/24 16:28:32 by vserra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template<typename T>
+class	MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(void) {}
+		MutantStack(MutantStack const & src) : std::stack<T>(src) {}
+		virtual ~MutantStack(void) {}
+
+		MutantStack<T>	&operator=(MutantStack const & rhs)
+		{
+			if (this != &rhs)
+				std::stack<T>::operator=(rhs);
+			return (*this);
+		}
+
+		typedef typename	std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
+		iterator	end(void)
+		{
+			return (this->c.end());
+		}
+};
+
+#endif
